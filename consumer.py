30990ecde3cff5e2df488258972a71797376e286
Nick Silva <silva.d.nicholas@gmail.com>
2019-03-02 15:26:57 -0600

    lolol

--- consumer.py
+++ consumer.py
@@ -23,70 +23,96 @@ class consumer:
     range2 = range(0, price2, step)
     range3 = range(0, price3, step)
 
-    for i in range(500): #number of iterations at each price set
-      if(epsilon > random()): #should we explore or not
-        if i % 2 == 0:
-          self.sell(random.randint(0,len(q_table[0])-1))
-        else:
-          self.buy_rand(ben1, ben2, ben3)
-      else:
-        if i % 2 == 0:
-          self.sell(max(q_table[0]))
-        else:
-          self.buy(ben1, ben2, ben3)
 
-      epsilon -= .001
-      learning_rate -= .002
-      discount_rate -= .002
-      print(q_table)
+                    for i in range(500): #number of iterations at each price set
+                      if(self.epsilon > random.random()): #should we explore or not
+                        if i % 2 == 0:
+                          self.sell(random.randint(0,len(self.q_table[0])-1))
+                        else:
+                          self.buy_rand(ben1, ben2, ben3)
+                      else:
+                        if i % 2 == 0:
+                          self.sell(self.max_col(self.q_table[0]))
+                        else:
+                          self.buy(ben1, ben2, ben3)
+
+      self.epsilon -= .001
+      self.learning_rate -= .002
+      self.discount_rate -= .002
+      print(self.q_table)
 
   def sell(self, col):
-    daily_effort -= col
-    wealth += col
-    q_table[0][col] = q_table[0][col] + learning_rate \
-      * (daily_effort*(1 - discount_rate) + discount_rate \
-      * q_table[1][max(q_table[1][max(q_table[1]):(wealth + 1)])] - q_table[0][col]) #mightbe broke
+    self.daily_effort -= col
+    self.wealth += col
+    self.q_table[0][col] = self.q_table[0][col] + self.learning_rate * (self.daily_effort*(1 - self.discount_rate) + self.discount_rate * self.q_table[1][self.max_col(self.q_table[1][:(self.wealth + 1)])] - self.q_table[0][col]) #mightbe broke
+    return 0
 
-  def buy_rand(b1, b2, b3):
-    if wealth == 0:
-        q_table[1][0] = q_table[1][0] + learning_rate*(-1000 + \
-        discount_rate*(q_table[2][max(q_table[2]):(wealth + 1)]) - q_table[1][0])
+  def buy_rand(self, b1, b2, b3):
+    if self.wealth == 0:
+        self.q_table[1][0] = self.q_table[1][0] + self.learning_rate*(-1000 + \
+        self.discount_rate*(self.q_table[2][self.max_col(self.q_table[2][:(self.wealth + 1)])] - self.q_table[1][0]))
     else:
-        x = random.randint(0, wealth)
-        wealth -= x
-        q_table[1][x] = q_table[1][x] + learning_rate*(x * b1 + \
-        discount_rate*(q_table[2][max(q_table[2]):(wealth + 1)]) - q_table[1][x])
+        x = random.randint(0, self.wealth)
+        if x > 4:
+            x = 4
+        self.wealth -= x
+        print("line 57", x, self.wealth)
+        self.q_table[1][x] = self.q_table[1][x] + self.learning_rate*(x * b1 + \
+        self.discount_rate*(self.q_table[2][self.max_col(self.q_table[2][:(self.wealth + 1)])] - self.q_table[1][x]))
 
-        x = random.randint(0, wealth)
-        wealth -= x
-        q_table[2][x] = q_table[2][x] + learning_rate*(x * b2 + \
-        discount_rate*(q_table[3][max(q_table[3]):(wealth + 1)]) - q_table[2][x])
+        if x == 0:
+            return
 
-        x = random.randint(0, wealth)
-        wealth -= x
-        q_table[3][x] = q_table[3][x] + learning_rate*(x * b3 + \
-        discount_rate*(q_table[1][max(q_table[1]):(q_table[0]\
-        [max(q_table[0])] + wealth + 1)]) - q_table[1][x])
+        x = random.randint(0, self.wealth)
+        if x > 4:
+            x = 4
+        self.wealth -= x
+        self.q_table[2][x] = self.q_table[2][x] + self.learning_rate*(x * b2 + \
+        self.discount_rate*(self.q_table[3][self.max_col(self.q_table[3][:(self.wealth + 1)])] - self.q_table[2][x]))
 
-  def buy(b1, b2, b3):
-    if wealth == 0:
-        q_table[1][0] = q_table[1][0] + learning_rate*(-1000 + \
-        discount_rate*(q_table[2][max(q_table[2]):(wealth + 1)]) - q_table[1][0])
+        if x == 0:
+            return
 
-    x = q_table[1][max(q_table[1]):wealth]
-    wealth -= x
-    q_table[1][x] = q_table[1][x] + learning_rate*(x * b1 + \
-    discount_rate*(q_table[2][max(q_table[2]):(wealth + 1)]) - q_table[1][x])
+        x = random.randint(0, self.wealth)
+        if x > 4:
+            x = 4
+        self.wealth -= x
+        print("wealth",self.wealth)
+        self.q_table[3][x] = self.q_table[3][x] + self.learning_rate*(x * b3 + \
+        self.discount_rate*(self.q_table[1][self.max_col(self.q_table[1][:\
+        self.max_col(self.q_table[0]) + self.wealth + 1])]) - self.q_table[1][x])
 
-    if wealth >= 0:
-        x = q_table[2][max(q_table[2]):wealth]
-        wealth -= x
-        q_table[2][x] = q_table[2][x] + learning_rate*(x * b2 + \
-        discount_rate*(q_table[3][max(q_table[3]):(wealth + 1)]) - q_table[2][x])
+  def buy(self, b1, b2, b3):
+    if self.wealth == 0:
+        self.q_table[1][0] = self.q_table[1][0] + self.learning_rate*(-100 + \
+        self.discount_rate*(self.q_table[2][self.max_col(self.q_table[2][:(self.wealth + 1)])] - self.q_table[1][0]))
 
-    if wealth >= 0:
-        x = q_table[3][max(q_table[3]):wealth]
-        wealth -= x
-        q_table[3][x] = q_table[3][x] + learning_rate*(x * b3 + \
-        discount_rate*(q_table[1][max(q_table[1]):(q_table[0]\
-        [max(q_table[0])] + wealth + 1)]) - q_table[1][x])
+    print(self.wealth)
+    x = self.max_col(self.q_table[1][:self.wealth + 1])
+    self.wealth -= x
+    self.q_table[1][x] = self.q_table[1][x] + self.learning_rate*(x * b1 + \
+    self.discount_rate*(self.q_table[2][self.max_col(self.q_table[2][:(self.wealth + 1)])] - self.q_table[1][x]))
+
+    if self.wealth >= 0:
+        x = self.max_col(self.q_table[2][:self.wealth + 1])
+        self.wealth -= x
+        self.q_table[2][x] = self.q_table[2][x] + self.learning_rate*(x * b2 + \
+        self.discount_rate*(self.q_table[3][self.max_col(self.q_table[3][:(self.wealth + 1)])] - self.q_table[2][x]))
+
+    if self.wealth >= 0:
+        x = self.max_col(self.q_table[3][:self.wealth + 1])
+        self.wealth -= x
+        print("line 101: ", x,self.wealth)
+        self.q_table[3][x] = self.q_table[3][x] + self.learning_rate*(x * b3 + \
+        self.discount_rate*(self.q_table[1][self.max_col(self.q_table[1][:\
+        self.max_col(self.q_table[0]) + self.wealth + 1])]) - self.q_table[1][x])
+
+  def max_col(self, stuff):
+    max = stuff[0]
+    max_col_index = 0
+    for i in range(1, len(stuff)):
+      if stuff[i] > max:
+        max = stuff[i]
+        max_col_index = i
+
+    return max_col_index
---
+++ main.py
@@ -0,0 +1,4 @@
+import consumer
+
+run = consumer.consumer()
+run.consume(1,1,1,1,1,1,1)
